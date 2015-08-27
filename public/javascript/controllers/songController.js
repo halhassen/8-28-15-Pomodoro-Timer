b2f90263b2064f0a8762edbb0eb9208ac237235d
Hamad Alhassen <halhassen@gmail.com>
2015-08-27 14:20:30 -0700

    Added more functionality

--- public/javascript/controllers/songController.js
+++ public/javascript/controllers/songController.js
@@ -1,9 +1,9 @@
 angular.module("app").controller("SongController", ["SongFactory", "$modal", function(SongFactory, $modal){
-  var vm = this;
+  /*var vm = this;
   var getSongs = function() {
-      SongFactory.getSongs().then(function(data){
-         vm.playlist = data;
-      });
+    SongFactory.getSongs().then(function(data){
+     vm.playlist = data;
+   });
   }
   getSongs();
   vm.song = {};
@@ -16,32 +16,87 @@ angular.module("app").controller("SongController", ["SongFactory", "$modal", fun
     });
     createModal.result.then(function(songCreated){
       SongFactory.addSong(songCreated).then(function(){
-          getSongs();
+        getSongs();
       })
     })
   }
   vm.deleteSong = function(song_id) {
-      SongFactory.deleteSong(song_id).then(function(){
-          getSongs();
-      })
+    SongFactory.deleteSong(song_id).then(function(){
+      getSongs();
+    })
   }
   vm.openEditModal = function(idx) {
-      var editModal = $modal.open({
-          templateUrl: 'javascript/templates/editSong.html',
-          controller: 'EditModalController',
-          controllerAs: "vm",
-          size: "lg",
-          resolve: {
-              song: function() {
-                  return SongFactory.playlist[idx];
-              }
-          }
-      });
-      editModal.result.then(function(songEdited){
-          SongFactory.editSong(songEdited).then(function(){
-              getSongs();
-          })
+    var editModal = $modal.open({
+      templateUrl: 'javascript/templates/editSong.html',
+      controller: 'EditModalController',
+      controllerAs: "vm",
+      size: "lg",
+      resolve: {
+        song: function() {
+          return SongFactory.playlist[idx];
+        }
+      }
+    });
+    editModal.result.then(function(songEdited){
+      SongFactory.editSong(songEdited).then(function(){
+        getSongs();
       })
+    })
   }
 
+}]);
+*/
+//practice
+
+var vm = this;
+
+var getSongs = function() {
+  SongFactory.getSongs().then(function(data) {
+    vm.playlist = data
+  });
+}
+getSongs();
+
+vm.song = {};
+
+vm.openCreateModal = function() {
+  var createModal = $modal.open({
+    templateUrl: "javascript/templates/createSong.html",
+    controller: "CreateModalController",
+    controllerAs: "vm",
+    size: "lg"
+  });
+  createModal.result.then(function(songCreated) {
+    SongFactory.addSong(songCreated).then(function() {
+      getSongs();
+    })
+  })
+};
+
+vm.deleteSong = function(song_id) {
+  SongFactory.deleteSong(song_id).then(function() {
+    getSongs();
+  })
+};
+
+
+
+vm.openEditModal = function(idx) {
+  var editModal = $modal.open({
+    templateUrl: 'javascript/templates/editSong.html',
+    controller: 'EditModalController',
+    controllerAs: "vm",
+    size: "lg",
+    resolve: {
+      song: function() {
+        return SongFactory.playlist[idx];
+      }
+    }
+  });
+  editModal.result.then(function(songEdited){
+    SongFactory.editSong(songEdited).then(function(){
+      getSongs();
+    })
+  })
+}
 }]);
